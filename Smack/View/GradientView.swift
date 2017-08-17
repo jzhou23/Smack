4ca85a1e9978aa6219785b48b4338b0f2cce0ca9
jzhou <jzhou23@stevens.edu>
2017-08-17 10:52:59 -0400

    ChannelVC UI

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Jiahuang Zhou on 8/11/17.
+//  Copyright © 2017 jzhou23. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    // change inside a storyboard dynamically
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottonColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottonColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
